9fd7fd79d3562e3ebefd9f8b591dd7fab7c86bde
Mason <chtuff@gmail.com>
2024-01-26 22:02:22 -0500

    solution

---
+++ sumvsXOR.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'sumXor' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER n as parameter.
+     */
+
+    public static long sumXor(long n) {
+        long zeros = Long.toBinaryString(n).length() - Long.bitCount(n);
+        return (n > 0) ? 1L << zeros : 1;
+    }
+    
+
+}
